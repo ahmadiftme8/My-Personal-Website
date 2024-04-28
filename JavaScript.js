ccfbea2ee0f3f23a434fd798356a22497dfd41d9
ahmadiftme8 <ahmadiftme99@gmail.com>
2024-04-28 15:14:23 +0330

    initializing the menu aside

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,19 @@
 const toggleSwitch = document.querySelector('.checkbox');
 
+/* navigtion section codes */
+const menuBarElement = document.querySelector('.menu-bar-container');
+const barsElements = document.querySelector('.menu-bar-container div')
+const overlay = document.querySelector('.nav-overlay');
+const nav1 = document.querySelector('.nav1');
+const nav2 = document.querySelector('.nav2');
+const nav3 = document.querySelector('.nav3');
+const nav4 = document.querySelector('.nav4');
+
+
+
+
+
+
 /* JSON.parse(localStorage.getItem('theme')); */
 
 //switch theme dynamically
@@ -24,5 +38,29 @@ if(currentTheme){
     document.documentElement.setAttribute('data-theme', currentTheme);
 }
 
+
+
+//menu bar js
+
+function toggleNav() {
+    //toggle : menu bars/ open/colse
+    menuBarElement.classList.toggle('change');
+    //toggle: menu active
+    overlay.classList.toggle('overlay-active');
+    if(overlay.classList.contains('overlay-active')){
+        overlay.classList.add('overlay-slide-right');
+        overlay.classList.remove('overlay-slide-left');
+
+        
+    }else{
+        overlay.classList.add('overlay-slide-left');
+        overlay.classList.remove('overlay-slide-right');
+    }
+  }
+
+  menuBarElement.addEventListener('click', toggleNav)
+
+
+//menu bar js
 
 
